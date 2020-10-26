d1ad139a29153f416479c1eebbb3d1005bac9f92
Bakuru <negabakuru@gmail.com>
2020-10-26 06:12:21 +0100

    Refresh script uses UTF-8

--- RefreshImagesW.cmd
+++ RefreshImagesW.cmd
@@ -5,6 +5,9 @@ for /L %%n in (1 1 500) do if "!__cd__:~%%n,1!" neq "" set /a "len=%%n+7+1"
 :+7 is for stripping 'images' from the path
 setlocal DisableDelayedExpansion
 
+:set encoding to UTF-8
+chcp 65001
+
 for /F "TOKENS=*" %%g in ('DIR /a-D /S /B images') do (
   set "absPath=%%g"
   setlocal EnableDelayedExpansion
