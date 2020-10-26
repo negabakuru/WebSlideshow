a8f15e865bc820c94f08ab8b69e28fc89ad7a66f
Bakuru <negabakuru@gmail.com>
2020-10-26 05:39:18 +0100

    Refresh script is recursive on one level

---
+++ RefreshImagesW NoRecurs.cmd
@@ -0,0 +1,3 @@
+echo let imageNamesStr = ` > images/images.js
+dir images /a-D /S /B >> images/images.js
+echo ` >> images/images.js
--- RefreshImagesW.cmd
+++ RefreshImagesW.cmd
@@ -1,3 +1,17 @@
 echo let imageNamesStr = ` > images/images.js
-dir images /b >> images/images.js
+
+setlocal EnableDelayedExpansion
+for /L %%n in (1 1 500) do if "!__cd__:~%%n,1!" neq "" set /a "len=%%n+7+1"
+:+7 is for stripping 'images' from the path
+setlocal DisableDelayedExpansion
+
+for /F "TOKENS=*" %%g in ('DIR /a-D /S /B images') do (
+  set "absPath=%%g"
+  setlocal EnableDelayedExpansion
+  set "absPath=!absPath:\=/!"
+  set "relPath=!absPath:~%len%!"
+  echo(!relPath! >> images/images.js
+  endlocal
+)
+
 echo ` >> images/images.js
